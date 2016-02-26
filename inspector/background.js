df750db17e9a9a511205b6fcb5388bcf41e078cd
Suyeon Kang <skang23@wustl.edu>
2016-02-26 11:22:15 -0600

    add bootstrap

--- inspector/background.js
+++ inspector/background.js
@@ -1,7 +1,44 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+var allWindows;
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url:chrome.extension.getURL("test.html")});
+//  chrome.tabs.create({url:chrome.extension.getURL("test.html")});
+  chrome.windows.getAll({populate: true}, function(windows){
+  	allWindows = windows;
+  	  windows.forEach(function(window){
+
+  	window.tabs.forEach(function(tab){
+      //collect all of the urls here, I will just log them instead
+      console.log(tab.url);
+    });
+      });
+
+  	var flag = false;
+  	for(var i = 0 ; i <allWindows.length;i++){
+  		var w = allWindows[i];
+  		console.log("this");
+  		console.log(w.tabs[0].title);
+  		if(w.tabs[0].title === "Tab Manager"){
+  			console.log("tab manager exist");
+  			flag = true;
+  			chrome.windows.update(w.id,{"focused": true}, null);
+  			break;
+  		}
+  	}
+  	if(flag === false){
+  		  chrome.windows.create({
+  	'type':"panel",
+  	'url':chrome.extension.getURL("test.html"),
+  	'width':300,
+  	'height':500
+  });
+  	}
+  });
+  // chrome.windows.create({
+  // 	'type':"panel",
+  // 	'url':chrome.extension.getURL("test.html"),
+  // 	'width':300,
+  // 	'height':500
+  // });
 });
--- inspector/test.js
+++ inspector/test.js
@@ -3,6 +3,47 @@ var allTabs;
 document.getElementById("create").addEventListener("click", createTab);
 document.getElementById("window").addEventListener("click", createWindow);
 document.getElementById("restore").addEventListener("click", restoreTab);
+document.getElementById("reduce").addEventListener("click", reduce);
+
+function saveTabs() {
+
+console.log("reduce");
+  var x = document.getElementsByClassName("reduce");
+  for(var i=0;i<x.length;i++){
+    if(x[i].checked){
+      var ID=parseInt(x[i].id);
+      removeTabWithId(ID);
+
+    }
+    console.log(x[i].checked);
+  }
+
+        // Get a value saved in a form.
+        var theValue = textarea.value;
+        // Check that there's some code there.
+        if (!theValue) {
+          message('Error: No value specified');
+          return;
+        }
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'value': theValue}, function() {
+          // Notify that we saved.
+          message('Settings saved');
+        });
+  }
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (key in changes) {
+          var storageChange = changes[key];
+          console.log('Storage key "%s" in namespace "%s" changed. ' +
+                      'Old value was "%s", new value is "%s".',
+                      key,
+                      namespace,
+                      storageChange.oldValue,
+                      storageChange.newValue);
+        }
+      });
+
 
 function restoreTab(){
 	chrome.sessions.restore(null,null);
@@ -12,25 +53,75 @@ function restoreTab(){
 function alertMe(){
 	alert('remove');
 }
+var allWindows;
+chrome.windows.getAll({populate: true}, function(windows){
+    allWindows = windows;
+      
+    var flag = false;
+    for(var i = 0 ; i <allWindows.length;i++){
+      console.log(allWindows.length);
+      var w = allWindows[i];
+      var _tabs = w.tabs;
+      var windowHTML='<div> Window '+i;
+      document.getElementById("tabs").innerHTML+=windowHTML+'<br>';
+
+      for(var j=0;j<_tabs.length;j++){
+      var title=_tabs[j].title;
+      var buttonHTML='<input type="checkbox" class="reduce" id="'+_tabs[j].id+'">';
+      document.getElementById("tabs").innerHTML+=buttonHTML+title+'<br>';
+    }
+    windowHTML='</div>';
+      document.getElementById("tabs").innerHTML+=windowHTML+'<br>';
+
+    }
+    
+  });
+
 //.addEventListener("click", removeTab);
- chrome.tabs.query({}, function (tabs) {
- 	allTabs=tabs;
- 		for(var i=0;i<tabs.length;i++){
- 			var url=tabs[i].url;
- 			var buttonHTML='<button class="remove" id="'+tabs[i].id+'">Remove</button>';
- 			document.getElementById("tabs").innerHTML+=buttonHTML+url+'<br>';
- 		}
- 		var removes=document.getElementsByClassName("remove");
-for(var i=0;i<removes.length;i++){
-	removes[i].addEventListener("click", removeTab);
-}
-        var url = tabs[0].url;
-        console.log("URL from main.js", url);
-    });
+ // chrome.tabs.query({}, function (tabs) {
+ // 	allTabs=tabs;
+ // 		for(var i=0;i<tabs.length;i++){
+ // 			var title=tabs[i].title;
+ // 			var buttonHTML='<input type="checkbox" class="reduce" id="'+tabs[i].id+'">';
+ // 			document.getElementById("tabs").innerHTML+=buttonHTML+title+'<br>';
+ // 		}
+//  		var removes=document.getElementsByClassName("remove");
+// for(var i=0;i<removes.length;i++){
+// 	removes[i].addEventListener("click", removeTab);
+// }
+    //     var url = tabs[0].url;
+    //     console.log("URL from main.js", url);
+    // });
 
 // function createTab(){
 // 	chrome.tabs.create(null, null) ;
 // }
+
+function reduce(){
+	console.log("reduce");
+	var x = document.getElementsByClassName("reduce");
+	for(var i=0;i<x.length;i++){
+		if(x[i].checked){
+			var ID=parseInt(x[i].id);
+			removeTabWithId(ID);
+
+		}
+		console.log(x[i].checked);
+	}
+
+}
+
+function removeTabWithId(tabId) {
+console.log("ad");
+  try {
+    chrome.tabs.remove(tabId, function() {
+      console.log('tab: ' + tabId + ' removed.');
+      chrome.tabs.reload();
+    });
+  } catch (e) {
+    alert(e);
+  }
+}
 
 function removeTab() {
 	var tabId=parseInt(this.id);
